00a449c15eca5fa07ed2cf83f96caa7f26f07dc1
Leon Chan <leon.chan@thisisgusto.com>
2024-01-23 18:50:26 +0800

    remote commit (WIP)

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,30 @@
 import fs from 'fs';
 import * as vscode from 'vscode';
+import { Queue } from './data_structure/queue';
 import { ImportPathFixer } from './import_path_fixer';
 
-export function activate(context: vscode.ExtensionContext) {
-	let renameEvent = vscode.workspace.onDidRenameFiles((e) => {
-		e.files.forEach((file) => {
-			console.log(`File has been renamed from ${file.oldUri.fsPath} to ${file.newUri.fsPath}`);
 
-			onPathChanged(file.oldUri.fsPath, file.newUri.fsPath);
+let queue = new Queue<() => Promise<void>>();
+
+let renameCounter = 0;
+
+export function activate(context: vscode.ExtensionContext) {
+	let didRenameEvent = vscode.workspace.onDidRenameFiles((e) => {
+		renameCounter--;
+		e.files.forEach((file) => {
+			queue.enqueue(
+				() => onPathChanged(file.oldUri.fsPath, file.newUri.fsPath)
+			);
+			processQueue();
 		});
 	});
 
-	context.subscriptions.push(renameEvent);
+	let willRenameEvent = vscode.workspace.onWillRenameFiles((e) => {
+		renameCounter++;
+	});
+
+
+	context.subscriptions.push(didRenameEvent);
 }
 
 async function onPathChanged(oldPath: string, newPath: string) {
@@ -21,7 +34,7 @@ async function onPathChanged(oldPath: string, newPath: string) {
 
 	let oldSubPath = getSubPathAfterLib(oldPath);
 	let newSubPath = getSubPathAfterLib(newPath);
-	let fixer = new ImportPathFixer(oldSubPath, newSubPath);
+	let fixer = new ImportPathFixer('/' + oldSubPath + '/', '/' + newSubPath + '/');
 	await fixer.executeImportFixes();
 }
 
@@ -35,3 +48,17 @@ function getSubPathAfterLib(filePath: string): string {
 	return parts.length > 1 ? parts[1] : '';
 }
 //#endregion
+
+async function processQueue() {
+	while (!queue.isEmpty()) {
+		//This does not work, coz rename counter is possible to increment when the task is executing
+		if (renameCounter > 0) {
+			continue;
+		}
+		const task = queue.dequeue();
+		if (task) {
+			await task();
+		}
+	}
+
+}
